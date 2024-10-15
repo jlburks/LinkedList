5695b684272af59def44b768e35b41dfb1cd1ec0
jlburks <jaren.burks1@gmail.com>
2024-10-14 23:16:34 -0500

    add node initial commit incomplete

--- LinkedList.java
+++ LinkedList.java
@@ -1,3 +1,20 @@
 public class LinkedList {
+    Node head;
+
+    public static void main(){
+        LinkedList llist = new LinkedList();
+
+    }
+
+    void addNode(){
+        if (head == null){
+            head = new Node();
+        } else {
+            // begin the chain of checking if next == null and inserting new node
+        }
+    }
+    void deleteNode(){
+
+    }
     
 }
--- Main.java
+++ Main.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 class Main {
+    String userinput = getUserInput();
     public static void main(String[] args){
-        
+        Main app = new Main();
+        while (app.userinput != "n"){
+            // preform action
+        }
+        // show llist and stats
     }
+    
+
+    String getUserInput(){
+        System.out.println("a:insert b:remove c:display");
+        Scanner action = new Scanner(System.in);
+
+        String x = action.nextLine();
+        return x;
+    }
+    
 }
